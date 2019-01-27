8cbfc2bd5b8856823ff487c9e87381421a0fad7c
yamanoku <0910yama@gmail.com>
2019-01-28 01:09:34 +0900

    add eslint-plugin-vue-a11y

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,15 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
+    'plugin:vue-a11y/base',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue',
+    'vue-a11y'
   ],
   // add your custom rules here
   rules: {
