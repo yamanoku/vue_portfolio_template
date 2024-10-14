c55aabfeb47a055fa59e1065b394bb0e747f6faf
yamanoku <0910yama@gmail.com>
2024-10-14 09:59:00 +0900

    vue-cli => vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [vue(), VitePWA()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
