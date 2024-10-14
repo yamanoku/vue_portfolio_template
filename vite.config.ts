80c2d505f5a314a9a06690b9f4c398e5ca429a3e
yamanoku <0910yama@gmail.com>
2024-10-14 11:12:03 +0900

    update: pwa vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,39 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+      manifest: {
+        name: "vue-portfolio-templete",
+        short_name: "vue-portfolio-templete",
+        description: "vue.js products --- portfolio template",
+        theme_color: "#ffffff",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
 });
